70d621f3422e05d0b3b873dc13c056177a731bad
MahdiyDev <mahdiydev@gmail.com>
2025-01-29 11:53:36 +0500

    fixed: file naming

--- hash_table/example/key_type.c
+++ hash_table/example/key_type.c
@@ -3,20 +3,50 @@
 #define HT_IMPLEMENTATION
 #include "../hash_table.h"
 
-#define ht_insert_int(ht, key, value) do { int _v = value; ht_insert(ht, key, &_v, sizeof(int)); } while(0)
-#define ht_search_int(ht, key) *(int*)ht_search(ht, key)
+struct vector { int x; int y; };
+
+#define ht_insert_vec_int(ht, key, value) do { \
+    int _v = value; \
+    struct vector _vk = key; \
+    char* _key = ht_to_char(&_vk, sizeof(_vk)); \
+    ht_insert(ht, _key, &_v, sizeof(int)); \
+    free(_key); \
+} while(0)
+#define ht_search_vec_int(ht, key) ({ \
+    struct vector _vk = key; \
+    char* _key = ht_to_char(&_vk, sizeof(_vk)); \
+    int _r = *(int*)ht_search(ht, _key); \
+    free(_key); \
+    _r; /* return value */ \
+})
 
 int main()
 {
+    struct vector v = {12, 21};
+    char* r = ht_to_char(&v, sizeof(v));
+
+    printf("bin data: { ");
+    for (int i = 0; i < sizeof(v); i++) {
+        printf("%d ", r[i]);
+    }
+    printf("}\n");
+
+    free(r);
+
     hash_table* ht = ht_init();
 
-    ht_insert_int(ht, "hello", 123);
-    ht_insert_int(ht, "hello1", 13);
-    ht_insert_int(ht, "hello2", 43);
+    struct vector vec1 = {1, 2};
+    ht_insert_vec_int(ht, vec1, 123);
 
-    printf("[1] %d\n", ht_search_int(ht, "hello"));
-    printf("[2] %d\n", ht_search_int(ht, "hello1"));
-    printf("[3] %d\n", ht_search_int(ht, "hello2"));
+    struct vector vec2 = {5, 2};
+    ht_insert_vec_int(ht, vec2, 13);
+    
+    struct vector vec3 = {5, 3};
+    ht_insert_vec_int(ht, vec3, 43);
+
+    printf("[1] %d\n", ht_search_vec_int(ht, vec1));
+    printf("[2] %d\n", ht_search_vec_int(ht, vec2));
+    printf("[3] %d\n", ht_search_vec_int(ht, vec3));
 
     ht_free(ht);
 }
